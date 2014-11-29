44f5e71207d5fd8e41616b58fcde950a19961c95
Petr Kessler <kesspess@gmail.com>
2014-11-29 17:28:04 +0100

    GA rendered only on remote

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo mtime('screen.css') ?>">
 
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
+<?php if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1') { ?>
 		<script type="text/javascript">(function(a,e,f,g,b,c,d){a.GoogleAnalyticsObject=b;a[b]=a[b]||function(){(a[b].q=a[b].q||[]).push(arguments)};a[b].l=1*new Date;c=e.createElement(f);d=e.getElementsByTagName(f)[0];c.async=1;c.src=g;d.parentNode.insertBefore(c,d)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-57169534-1","auto");ga("send","pageview");</script>
+<?php } ?>
 	</head>
 
 	<body>
